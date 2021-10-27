a8cbaaae69e5865ed247515a8be57065b15ed1d0
Ktcha <18026726100@163.com>
2021-10-28 00:08:07 +0800

    增加自定义配置项示例

--- springboot-init/src/main/java/com/controller/TestController.java
+++ springboot-init/src/main/java/com/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testhello;
+
     @RequestMapping("/yeah")
     private String Hello(){
-        return "yeah!";
+        return "yeah!" + testhello;
     }
 
 }
