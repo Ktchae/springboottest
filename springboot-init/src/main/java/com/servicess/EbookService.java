cecee1f2344a6aa467280a3e3c97c15fa560385c
Ktcha <18026726100@163.com>
2021-11-04 01:01:31 +0800

    制作CopyUtil封装BeanUtils

--- springboot-init/src/main/java/com/servicess/EbookService.java
+++ springboot-init/src/main/java/com/servicess/EbookService.java
@@ -1,15 +1,14 @@
 package com.servicess;
 
+import com.Util.CopyUtil;
 import com.domain.Ebook;
 import com.domain.EbookExample;
 import com.mapper.EbookMapper;
 import com.req.EbookReq;
 import com.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +23,19 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+
+        //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 
 }
