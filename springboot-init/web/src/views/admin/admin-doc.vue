6bc8d472f90194f17a108d5b656d1ec6e0ee9590
Ktcha <18026726100@163.com>
2021-11-25 15:11:27 +0800

    增加保存文档成功提示 编辑和新增的时候，先清空富文本框

--- springboot-init/web/src/views/admin/admin-doc.vue
+++ springboot-init/web/src/views/admin/admin-doc.vue
@@ -202,7 +202,9 @@ export default defineComponent({
         modalLoading.value = false;
         const data = response.data;   //data = commonResp
         if (data.success){
-          modalVisible.value = false;
+          // modalVisible.value = false;
+          message.success("保存成功！");
+
           //重新加载列表
           handleQuery();
         }else {
@@ -295,6 +297,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = (record:any) => {
+      //清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -311,6 +315,8 @@ export default defineComponent({
      * 新增
      */
     const add = () => {
+      //清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
