8cd991310e7c700ba4b86df59d92030384b8086f
Ktcha <18026726100@163.com>
2021-11-28 23:07:02 +0800

    密码加密传输和加密存储

--- springboot-init/src/main/java/com/controller/UserController.java
+++ springboot-init/src/main/java/com/controller/UserController.java
@@ -6,6 +6,7 @@ import com.resp.CommonResp;
 import com.resp.UserQueryResp;
 import com.resp.PageResp;
 import com.servicess.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -28,6 +29,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userservice.save(req);
         return resp;
