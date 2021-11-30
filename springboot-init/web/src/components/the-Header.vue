fd20409d3d04b4cbeffe10fa0393f8ceddfb77e7
Ktcha <18026726100@163.com>
2021-12-01 00:32:56 +0800

    前端显示登录昵称，遗留问题，刷新会没有

--- springboot-init/web/src/components/the-Header.vue
+++ springboot-init/web/src/components/the-Header.vue
@@ -22,7 +22,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{ user.name }}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
     </a-menu>
@@ -57,9 +60,14 @@ declare let KEY:any;
 export default defineComponent({
   name: 'the-Header',
   setup () {
+    //登录后保存
+    const user = ref();
+    user.value = {};
+
+    //用来登录
     const loginUser = ref({
-      loginName: "test",
-      password: "test"
+      loginName: "test123",
+      password: "test123"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -78,7 +86,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
-
+          user.value = data.content;
         } else {
           message.error(data.message);
         }
@@ -90,7 +98,8 @@ export default defineComponent({
       loginModalLoading,
       showLoginModal,
       loginUser,
-      login
+      login,
+      user
     }
   }
 });
