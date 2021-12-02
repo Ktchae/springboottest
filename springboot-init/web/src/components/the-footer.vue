af16751f76bbc7a3c75b70dd4156f13ca05da50d
Ktcha <18026726100@163.com>
2021-12-02 14:41:12 +0800

    vuex整合sessionStorage，解决刷新数据丢失问题。登录按钮位置还没调好

--- springboot-init/web/src/components/the-footer.vue
+++ springboot-init/web/src/components/the-footer.vue
@@ -1,12 +1,12 @@
 <template>
   <a-layout-footer style="text-align: center">
-    欢迎：{{user.name}} !
+    <span v-show="user.id">欢迎： {{ user.name }} !</span>
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import store from '@/store';
 import { defineComponent,computed } from 'vue';
+import store from '@/store';
 
 export default defineComponent({
   name: 'the-footer',
