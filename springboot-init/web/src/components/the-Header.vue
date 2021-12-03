06e0fc568b5e521dbd95a830c15485d81000d8a9
Ktcha <18026726100@163.com>
2021-12-03 15:30:43 +0800

    增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息

--- springboot-init/web/src/components/the-Header.vue
+++ springboot-init/web/src/components/the-Header.vue
@@ -22,9 +22,19 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" v-show="user.id" style="position:absolute;right:50px;">
+      <a class="login-menu" v-show="user.id" style="position:absolute;right:130px;">
         <span>您好：{{ user.name }}</span>
       </a>
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+      >
+        <a class="login-menu" v-show="user.id" style="position:absolute;right:50px;">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
       <a class="login-menu" v-show="!user.id" @click="showLoginModal" style="position:absolute;right:50px;">
         <span>登录</span>
       </a>
@@ -93,13 +103,28 @@ export default defineComponent({
       });
     };
 
+    // 退出登录
+    const logout = () => {
+      console.log("退出登录开始");
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     return {
       loginModalVisible,
       loginModalLoading,
       showLoginModal,
       loginUser,
       login,
-      user
+      user,
+      logout
     }
   }
 });
