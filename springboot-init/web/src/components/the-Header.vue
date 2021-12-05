5202ff00da2a646e3c820ee069943b3f6d9a2d17
Ktcha <18026726100@163.com>
2021-12-05 15:06:50 +0800

    前端未登录时，隐藏菜单

--- springboot-init/web/src/components/the-Header.vue
+++ springboot-init/web/src/components/the-Header.vue
@@ -10,21 +10,18 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/user">
+      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/user">用户管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook">
+      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category">
+      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" v-show="user.id" style="position:absolute;right:130px;">
-        <span>您好：{{ user.name }}</span>
-      </a>
       <a-popconfirm
           title="确认退出登录?"
           ok-text="是"
@@ -35,6 +32,9 @@
           <span>退出登录</span>
         </a>
       </a-popconfirm>
+      <a class="login-menu" v-show="user.id" style="position:absolute;right:130px;">
+        <span>您好：{{ user.name }}</span>
+      </a>
       <a class="login-menu" v-show="!user.id" @click="showLoginModal" style="position:absolute;right:50px;">
         <span>登录</span>
       </a>
