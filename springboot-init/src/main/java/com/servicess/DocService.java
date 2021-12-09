9795a72627eb112f0dddcc233de301f50d02e112
Ktcha <18026726100@163.com>
2021-12-10 01:20:36 +0800

    针对点赞通知功能，使用异步化够了，没必要引入MQ

--- springboot-init/src/main/java/com/servicess/DocService.java
+++ springboot-init/src/main/java/com/servicess/DocService.java
@@ -19,7 +19,6 @@ import com.req.DocQueryReq;
 import com.req.DocSaveReq;
 import com.resp.DocQueryResp;
 import com.resp.PageResp;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -53,8 +52,8 @@ public class DocService {
     @Resource
     public WsService wsService;
 
-    @Resource
-    private RocketMQTemplate rocketMQTemplate;
+//    @Resource
+//    private RocketMQTemplate rocketMQTemplate;
 
 
     public List<DocQueryResp> all(Long ebookId) {
@@ -168,8 +167,8 @@ public class DocService {
         // 推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
         String logId = MDC.get("LOG_ID");
-//        wsService.sendInfo("【" + docDb.getName() + "】被点赞！",logId);
-        rocketMQTemplate.convertAndSend("VOTE_TOPIC","【" + docDb.getName() + "】被点赞！");
+        wsService.sendInfo("【" + docDb.getName() + "】被点赞！",logId);
+//        rocketMQTemplate.convertAndSend("VOTE_TOPIC","【" + docDb.getName() + "】被点赞！");
     }
 
     public void updateEbookInfo(){
