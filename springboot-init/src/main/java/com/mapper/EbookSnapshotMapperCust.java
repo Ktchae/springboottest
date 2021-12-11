993e0a95d4be9f38e207d08931aa3c8e60918d39
Ktcha <18026726100@163.com>
2021-12-11 23:02:28 +0800

    后端增加获取统计数值接口

--- springboot-init/src/main/java/com/mapper/EbookSnapshotMapperCust.java
+++ springboot-init/src/main/java/com/mapper/EbookSnapshotMapperCust.java
@@ -1,7 +1,13 @@
 package com.mapper;
 
+import com.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 
 }
