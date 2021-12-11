516215c2de4d3ec17639b4867796044667382148
Ktcha <18026726100@163.com>
2021-12-11 23:17:37 +0800

    将欢迎文字提取成组件

--- springboot-init/web/src/views/Home.vue
+++ springboot-init/web/src/views/Home.vue
@@ -25,7 +25,7 @@
         :style="{ background: '#ffffff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用</h1>
+        <the-welcome></the-welcome>
       </div>
 
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
@@ -68,6 +68,7 @@ import {defineComponent, onMounted, ref} from 'vue';
 import axios from "axios";
 import {message} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
 // const listData: Record<string, string>[] = [];
 //
 // for (let i = 0; i < 23; i++) {
@@ -84,6 +85,9 @@ import {Tool} from "@/util/tool";
 
 export default defineComponent({
   name: 'Home',
+  components:{
+    TheWelcome
+  },
   setup() {
     const ebooks = ref();
     //const ebooks1 = reactive({books:[]});
