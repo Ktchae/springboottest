5b20355491e9ed14da1e6b85b99c1cad07d6acaa
Ktcha <18026726100@163.com>
2021-12-13 00:38:58 +0800

    BUG修复，如果一个电子书初始没有文档，则父文档选择框无法选中“无” 文案修改

--- springboot-init/web/src/views/About.vue
+++ springboot-init/web/src/views/About.vue
@@ -2,7 +2,7 @@
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
   <div class="about">
-    <h1>这是一个编程学习网站，包括前端、后端、数据和各框架等，持续更新！</h1>
+    <h1>这是一个编程学习网站，包括前端、后端、数据库和各框架等，持续更新！</h1>
   </div>
     </a-layout-content>
   </a-layout>
