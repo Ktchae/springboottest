43edd4d6d7264218026b1bd2aa120683fd5cb25e
Ktcha <18026726100@163.com>
2021-12-13 00:28:24 +0800

    关于我们文案修改

--- springboot-init/web/src/views/About.vue
+++ springboot-init/web/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>这是一个编程学习网站，包括前端、后端、数据和各框架等，持续更新！</h1>
   </div>
+    </a-layout-content>
+  </a-layout>
 </template>
