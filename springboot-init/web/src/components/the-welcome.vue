29f80c613338a05d47e8e22de9626f8e0c84653a
Ktcha <18026726100@163.com>
2021-12-20 20:14:12 +0800

    完善关于和欢迎页面

--- springboot-init/web/src/components/the-welcome.vue
+++ springboot-init/web/src/components/the-welcome.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div class="tip">
+      <div><b>网站说明：用于校园用户学习交流</b></div>
+    </div>
+
     <a-row>
       <a-col :span="24">
         <a-card>
@@ -7,14 +11,14 @@
             <a-col :span="8">
               <a-statistic title="总阅读量" :value="statistic.viewCount">
                 <template #suffix>
-                  <UserOutlined/>
+                  <UserOutlined />
                 </template>
               </a-statistic>
             </a-col>
             <a-col :span="8">
               <a-statistic title="总点赞量" :value="statistic.voteCount">
                 <template #suffix>
-                  <like-outlined/>
+                  <like-outlined />
                 </template>
               </a-statistic>
             </a-col>
@@ -24,7 +28,7 @@
                            suffix="%"
                            :value-style="{ color: '#cf1322' }">
                 <template #suffix>
-                  <like-outlined/>
+                  <like-outlined />
                 </template>
               </a-statistic>
             </a-col>
@@ -40,14 +44,14 @@
             <a-col :span="12">
               <a-statistic title="今日阅读" :value="statistic.todayViewCount" style="margin-right: 50px">
                 <template #suffix>
-                  <UserOutlined/>
+                  <UserOutlined />
                 </template>
               </a-statistic>
             </a-col>
             <a-col :span="12">
               <a-statistic title="今日点赞" :value="statistic.todayVoteCount">
                 <template #suffix>
-                  <like-outlined/>
+                  <like-outlined />
                 </template>
               </a-statistic>
             </a-col>
@@ -64,7 +68,7 @@
                   :value-style="{ color: '#0000ff' }"
               >
                 <template #suffix>
-                  <UserOutlined/>
+                  <UserOutlined />
                 </template>
               </a-statistic>
             </a-col>
@@ -89,7 +93,7 @@
     </a-row>
     <br>
     <a-row>
-      <a-col :span="24">
+      <a-col :span="24" id="main-col">
         <div id="main" style="width: 100%;height:300px;"></div>
       </a-col>
     </a-row>
@@ -104,7 +108,7 @@ declare let echarts: any;
 
 export default defineComponent({
   name: 'the-welcome',
-  setup() {
+  setup () {
     const statistic = ref();
     statistic.value = {};
     const getStatistic = () => {
@@ -121,17 +125,21 @@ export default defineComponent({
           const now = new Date();
           const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
           // console.log(nowRate)
-          statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease
-              / nowRate));
+          statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
           // todayViewIncreaseRate：今日预计增长率
-          statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease)
-              / statisticResp[0].viewIncrease * 100;
+          statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
           statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
         }
       });
     };
 
     const init30DayEcharts = (list: any) => {
+      // 发布生产后出现问题：切到别的页面，再切回首页，报表显示不出来
+      // 解决方法：把原来的id=main的区域清空，重新初始化
+      const mainDom = document.getElementById('main-col');
+      if (mainDom) {
+        mainDom.innerHTML = '<div id="main" style="width: 100%;height:300px;"></div>';
+      }
       // 基于准备好的dom，初始化echarts实例
       const myChart = echarts.init(document.getElementById('main'));
 
@@ -208,34 +216,6 @@ export default defineComponent({
       });
     };
 
-    const testEcharts = () => {
-      // 基于准备好的dom，初始化echarts实例
-      const myChart = echarts.init(document.getElementById('main'));
-
-      // 指定图表的配置项和数据
-      const option = {
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-          data:['销量']
-        },
-        xAxis: {
-          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      };
-
-      // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option);
-    };
-
     onMounted(() => {
       getStatistic();
       // testEcharts();
@@ -248,3 +228,15 @@ export default defineComponent({
   }
 });
 </script>
+<style scoped>
+.tip {
+  padding: 10px 5px;
+  margin-bottom: 20px;
+  border: 1px solid transparent;
+  background: linear-gradient(white,white) padding-box,repeating-linear-gradient(-45deg, black 0, black 25%, white 0, white 50%) 0/.6em .6em;
+  animation:ants 12s linear infinite;
+}
+.tip b{
+  color: red;
+}
+</style>
