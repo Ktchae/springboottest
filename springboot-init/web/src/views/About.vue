7c6eb6a8edad053edc05485bf011243c5ce3571a
Ktcha <18026726100@163.com>
2021-12-20 20:34:28 +0800

    接前面一条

--- springboot-init/web/src/views/About.vue
+++ springboot-init/web/src/views/About.vue
@@ -1,9 +1,61 @@
 <template>
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-  <div class="about">
-    <h1>这是一个编程学习网站，包括前端、后端、数据库和各框架等，持续更新！</h1>
-  </div>
+      <div class="about">
+        <h1>广东科技学院（Guangdong University of Science and Technology）
+          位于广东省东莞市，是国家教育部批准设立的一所以工学为主，
+          管理学、经济学、文学、艺术学等多学科发展的全日制普通本科院校。</h1>
+        <div><b>学院始建于2003年的东莞南博专修学院；2004年9月，升格为东莞南博职业技术学院；
+          2011年4月，升格为普通本科院校，更名为广东科技学院；
+          2015年取得学士学位授予权；2018年通过教育部本科教学工作合格评估。
+          2021年成功入选广东省硕士学位授予立项建设单位并获批设立广东省博士工作站。</b></div>
+        <div>
+          <b>学校相关：
+            <a href="http://www.gdust.edu.cn" target="_blank">
+              学校官网
+            </a>
+          </b>
+        </div>
+        <div>
+          <b>学校相关：
+            <a href="https://zs.gdust.edu.cn/" target="_blank">
+              招生消息网
+            </a>
+          </b>
+        </div>
+        <div>
+          <b>学校相关：
+            <a href="http://www.gdust.edu.cn/xygl/xyfj/" target="_blank">
+              校园风景
+            </a>
+          </b>
+        </div>
+        <br>
+        <div><b>招生热线：（0769）86211555 86211666 86211777
+          86211999 86211801 86211802<span style="color: rgb(194, 79, 74)"></span></b></div>
+        <div><b>传真：86211818</b></div>
+        <div><b>E-MAIL:zsgdst@126.com</b></div>
+        <div><b>地址:广东省东莞市南城区西湖路99号</b></div>
+        <div style="position:absolute;left:20px;">
+          <img src="/image/gdkjxy.png">
+        </div>
+      </div>
     </a-layout-content>
   </a-layout>
 </template>
+
+<style>
+.about {
+  line-height: 30px;
+  padding: 40px;
+  background-color: #f5f5f5;
+  font-size: 16px;
+}
+.about h2 {
+  margin-bottom: 30px;
+  font-weight: 700;
+}
+.about img {
+  margin-top: 10px;
+}
+</style>
